ef576bef41ca8ecd1a6ab12d44f79651b72d5652
Ravi Pandey <74792529+mrravipande@users.noreply.github.com>
2022-06-23 11:29:49 +0530

    Create CubeAndSquare.c

---
+++ CubeAndSquare.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	int n;
+	
+	printf("Enter a number:");
+	scanf("%d",&n);
+	
+	printf("Cube is %d\n",n*n*n);
+	printf("Square root is %d",n*n);
+  
+  return 0;
+}
