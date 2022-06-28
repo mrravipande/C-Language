2a1c53b0ee7bcee8f043dbcd0728f7fa241629f7
Ravi Pandey <74792529+mrravipande@users.noreply.github.com>
2022-06-28 15:54:45 +0530

    Create TableFunc.c

---
+++ TableFunc.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void table(int n);
+
+int main()
+{
+	printf("Enter table number:");
+	int num;
+	table(num);
+	
+	return 0;
+}
+
+void table(int n)
+{
+	for(int i=1; i<=10; i++)
+	{
+		printf("%d * %d = %d",n,i,n*i);
+	}
+	
+}
